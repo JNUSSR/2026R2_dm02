f66725a484385e5d36c64eb9d2c64c58495e0bfd
bees-creation <2991280008@qq.com>
2026-06-25 18:25:21 +0800

    夹爪移动45度

--- User_File/3_Module/Clamping/Clamping_controller.cpp
+++ User_File/3_Module/Clamping/Clamping_controller.cpp
@@ -73,6 +73,11 @@ void ClampingController::MoveToDockAngle(void)
     PlanToAngle(CLAMPING_TARGET_ANGLE_DOCK_RAD);
 }
 
+void ClampingController::MoveToAirtag(void)
+{
+    PlanToAngle(CLAMPING_TARGET_ANGLE_AIRTAG_RAD);
+}
+
 void ClampingController::OpenSolenoid(void)
 {
     Cylinder1_Push();
